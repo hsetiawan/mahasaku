6e2ddb9ad723c1b115abb64db0fed4260573c579
hsetiawan <hendset.ti04@gmail.com>
2019-11-20 08:47:01 +0700

    finish mahasaku

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,57 @@ $(".center").slick({
   dots: true,
   infinite: true,
   centerMode: true,
-  slidesToShow: 5,
-  slidesToScroll: 3
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  autoplay:true
 });
+
+function doSubscribe(){
+  var email = $("#email-subscribe").val();
+  var message = "Hei <b>"+email+"</b>. Kamu telah berhasil berlangganan. <br/>Kamu akan mendapatkan informasi menarik dari mahasaku";
+  if(email.length > 0 && email.includes("@")){
+    $("#exampleModal").modal({show:true});
+    $("#content-subscribe").html(message);
+  }else{
+    $("#email-subscribe").css("border","1px solid red");
+  }
+}
+
+$(document).on("input", ".numeric", function() {
+  this.value = this.value.replace(/\D/g,'');
+});
+
+
+function doNeedHelp(){
+ 
+    var nama = $("#helpNama").val();
+    var noTelp = $("#helpPhone").val();
+
+    if(nama.length > 0 && noTelp.length > 0){
+      $("#modalHelp").modal({show:true});
+    }else{
+      $("#needHelpForm.needs-validation").attr('class','was-validated');
+    }
+}
+
+function doCloseHelp(){
+  $("#needHelpForm").trigger('reset');
+  window.location.reload();
+}
+
+
+function doRegisterSuccess(){
+ 
+  var nama = $("#name").val();
+  var email = $("#email").val();
+  var password = $("#password").val();
+  var confPassword = $("#confPassword").val();
+  
+  console.log(nama.length);
+  if( confPassword.length > 0){
+    console.log("masuk");
+    $("#modalHelp").modal({show:true});
+  }else{
+    $("#register-form.needs-validation").attr('class','was-validated');
+  }
+}
---
+++ js/step-wizard.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+
+    var navListItems = $('div.setup-panel div a'),
+            allWells = $('.setup-content'),
+            allNextBtn = $('.nextBtn');
+
+    allWells.hide();
+
+    navListItems.click(function (e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+                $item = $(this);
+
+        if (!$item.hasClass('disabled')) {
+            navListItems.removeClass('btn-primary').addClass('btn-default');
+            $item.addClass('btn-primary');
+            allWells.hide();
+            $target.show();
+            $target.find('input:eq(0)').focus();
+        }
+    });
+
+    allNextBtn.click(function(){
+        var curStep = $(this).closest(".setup-content"),
+            curStepBtn = curStep.attr("id"),
+            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+            curInputs = curStep.find("input[type='text'],input[type='url'],input[type='email'],input[type='password']"),
+            isValid = true;
+    
+        var forms = document.getElementsByClassName('needs-validation');    
+
+        $(".form-group").removeClass("has-error");
+        for(var i=0; i<curInputs.length; i++){
+            if (!curInputs[i].validity.valid){
+                isValid = false;
+                $(curInputs[i]).closest(".form-group").addClass("has-error");
+                $("#register-form.needs-validation").attr('class','was-validated');
+            }
+        }
+
+        if (isValid)
+            nextStepWizard.removeAttr('disabled').trigger('click');
+    });
+
+    $('div.setup-panel div a.btn-primary').trigger('click');
+});
+
+
+function doRegisterSuccess(){
+    var nama = $("#name").val();
+    var email = $("#email").val();
+    var password = $("#password").val();
+    var confPassword = $("#confPassword").val();
+    
+  
+    if(nama.length > 0 && email.length > 0 && confPassword.length > 0){
+      $("#modalHelp").modal({show:true});
+    }else{
+      $("#register-form.needs-validation").attr('class','was-validated');
+    }
+  }
+
+  function doCloseHelp(){
+    $("#register-form").trigger('reset');
+    window.location.reload();
+  }  
